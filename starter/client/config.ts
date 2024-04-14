2df1d4aa533b3080fe1aec610c211a550b21e33e
hphuc3005 <hphuc3005@gmail.com>
2024-04-15 02:22:51 +0700

    Add config file for client

---
+++ starter/client/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'zgzus0z2qb'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-emxi252x7sgp2fcm.us.auth0.com', // Domain from Auth0
+  clientId: 'AFxX8csL1IZHzctt0IgtArZE49qRLKVL', // Client id from an Auth0 application
+  callbackUrl: 'http://localhost:3000/callback'
+}
